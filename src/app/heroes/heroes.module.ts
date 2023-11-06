ce42c6c4613eed170690b6149aebcefddf0ae33c
Alejandro Toledo <alejandro.toledo@devsu.com>
2023-11-06 15:43:24 -0500

    add search page with autocomplete

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -8,6 +8,7 @@ import { ListPageComponent } from './pages/list-page/list-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
 import { MaterialModule } from '../material/material.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -21,7 +22,9 @@ import { MaterialModule } from '../material/material.module';
   imports: [
     CommonModule,
     HeroesRoutingModule,
-    MaterialModule
+    MaterialModule,
+
+    ReactiveFormsModule
   ]
 })
 export class HeroesModule { }
